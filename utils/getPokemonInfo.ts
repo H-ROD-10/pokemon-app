bb9145d07a29578948b33f00475e4b5e9fef1fb0
H-ROD-10 <hector.rodriguezb@gmail.com>
2022-12-28 13:27:27 -0400

    Incremental Generation

--- utils/getPokemonInfo.ts
+++ utils/getPokemonInfo.ts
@@ -3,13 +3,19 @@ import { IPokemonDetailsResponse } from "../interfaces"
 
 export const getPokemonInfo = async(nameOrId: string) => {
   
+    try {
+        const { data } = await pokeApi.get<IPokemonDetailsResponse>(`/pokemon/${nameOrId}`)
 
-    const { data } = await pokeApi.get<IPokemonDetailsResponse>(`/pokemon/${nameOrId}`)
-
-    return {
-        id: data.id,
-        name: data.name,
-        sprites: data.sprites
+        return {
+            id: data.id,
+            name: data.name,
+            sprites: data.sprites
+        }
+       
+        
+    } catch (error) {
+       return null
     }
+   
 
 }
